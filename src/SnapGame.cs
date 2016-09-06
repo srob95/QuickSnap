019ba397c156734b8a92925b1800cff9623109bd
srob95 <robertson-sam@hotmail.com>
2016-09-06 18:31:54 +1000

    add

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -10,12 +10,14 @@ namespace CardGames
         {
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
-            SwinGame.BitmapSetCellDetails (cards, 167, 250, 13, 5, 53);      // set the cells in the bitmap to match the cards
-			SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
-		}
+            SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
+			SwinGame.BitmapSetCellDetails (cards, 167, 250, 13, 5, 53);      // set the cells in the bitmap to match the cards
+			
+			
 		/// CHECK
 		///	MERGE CONFLICT TESTER
 		/// CHECK
+		}
 		/// <summary>
 		/// Respond to the user input -- with requests affecting myGame
 		/// </summary>
